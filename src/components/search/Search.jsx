4945b90ff2a63bd639554d6be548bce119c339e6
Danil <dnllt@mail.ru>
2021-07-11 00:36:30 +0300

    11.07.21

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -3,6 +3,19 @@ import React, { Component } from "react";
 import "./search.css";
 
 export default class Search extends Component {
+  // componentDidMount() {
+  //   this.inputRef.focus();
+  // }
+
+  constructor() {
+    super();
+    this.inputRef = React.createRef(); //создаём референцию и записываем ее в переменную
+  }
+
+  componentDidMount() {
+    this.inputRef.current.focus();
+  }
+
   render() {
     return (
       <div className="movie-search">
@@ -11,6 +24,8 @@ export default class Search extends Component {
           placeholder="Tupe to search..."
           type="search"
           onChange={this.props.hendleOnChange}
+          // ref={(inputRef) => (this.inputRef = inputRef)}
+          ref={this.inputRef} //получаем доступ к элементу
         />
       </div>
     );
